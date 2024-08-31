6046c1d1b7baa7eb458bcc82f67607ab4753befe
behan05 <behankrbth@outlook.com>
2024-08-31 10:08:49 +0530

    Understand about type conversion

---
+++ 0.6 Type conversion/script.js
@@ -0,0 +1,19 @@
+/*
+Data conversion means to convert data type like string to number and number to string and so on.
+*/
+
+let score = 23; // Number
+// let name = "Behan"; // String
+let name = "Behan234"; // String
+let isValidScore = true; // Boolean
+
+// let convert = Number(name); // NaN(Not a Number)
+// let convert = Number(name); // NaN(Not a Number)
+// let convert = String(score);
+// let convert = String(isValidScore);
+// let convert = Boolean(score);
+// let convert = Boolean(name);
+let convert = Number(name);
+console.log(typeof convert);
+
+console.log(convert);
