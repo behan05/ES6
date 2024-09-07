f73b5a83eaa611cfe67d663f1225a8fd0bc9c44c
behan05 <behankrbth@outlook.com>
2024-09-08 01:53:39 +0530

    learn about map and filter method

---
+++ 1.5 Loops/0.5 High Order Method/script.js
@@ -0,0 +1,95 @@
+// map, filter, and reduce are array methods that allow you to process and manipulate arrays efficiently using callback functions.
+
+const myArray = [1, 2, 3, 4, 5];
+const double = myArray.map((i, index) => {
+  return i * 2;
+});
+// const fil = double.filter((i) => i >= 8);
+// console.log(fil);
+
+/*
+// forEach mehtod can not return value.
+let n = myArray.forEach((i) => {
+  return i;
+});
+
+console.log(n);
+*/
+
+const books = [
+  {
+    title: "book One",
+    genre: "history",
+    publish: 1995,
+    edition: 2005,
+  },
+  {
+    title: "book Two",
+    genre: "science",
+    publish: 2000,
+    edition: 2007,
+  },
+  {
+    title: "book Three",
+    genre: "biology",
+    publish: 1997,
+    edition: 2013,
+  },
+  {
+    title: "book Four",
+    genre: "computer vision",
+    publish: 1985,
+    edition: 2014,
+  },
+  {
+    title: "book Five",
+    genre: "Agreegetion",
+    publish: 2015,
+    edition: 2017,
+  },
+  {
+    title: "book One",
+    genre: "history",
+    publish: 2000,
+    edition: 2024,
+  },
+];
+
+/*
+books.map((i, index) => {
+  for (const key in i) {
+    console.log(key + " " + i[key]);
+    console.log();
+    console.log();
+  }
+});
+*/
+
+// *********  Filter  **************
+/*
+for (const element of books) {
+  for (const key in element) {
+    if (
+      Object.hasOwnProperty.call(element, key) &&
+      element[key] === "history"
+    ) {
+      console.log(key + " " + element[key]);
+    }
+  }
+}
+*/
+
+const filterData = books.filter((items) => items.genre === "history");
+// console.log(filterData);
+
+// for (const key in filterData) {
+//   if (Object.prototype.hasOwnProperty.call(filterData, key)) {
+//     const element = filterData[key];
+//     console.log(element);
+//   }
+// }
+
+
+// ***************  Reduce  **************************
+
+
