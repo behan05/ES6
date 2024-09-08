28b531cb70c49e4f151607f839e59cfae680bc93
behan05 <behankrbth@outlook.com>
2024-09-08 13:38:26 +0530

    start basic of DOM

---
+++ 1.6 DOM/0.1 Basic Of DOM/script.js
@@ -0,0 +1,7 @@
+// console.log(document.firstElementChild.lastElementChild.firstElementChild.firstChild)
+
+const logo = document.querySelector(".logo");
+
+logo.addEventListener("mouseover", () => {
+  logo.style.color = "lightseagreen";
+});
